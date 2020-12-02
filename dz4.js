fab378f09f036b6a3dabf540d0e75841c01979b9
FH-42 <romademianenko>
2020-12-02 22:07:23 +0200

    g

--- dz4.js
+++ dz4.js
@@ -1,14 +1,20 @@
 // 111111111111111111111
+// b = confirm('dz1');
+
 // while (b === false) {
-//     alert (b = confirm('dz1'));
+//     b = confirm('dz1')
 // }
+
 // 1111111111111111111
 
 
 // 22222222222222222222222222222
 // let c = +prompt('Enter your number');
-// let d = Array.apply(null, {length: c}).map(Number.call, Number);
-// let b = d.map(n => n ** 3);
+// let arr = [];
+// for (i = 0; i <= c; i++){
+//     arr[i]=i;
+// }
+// let b = arr.map(n => n ** 3);
 // console.log(b)
 // 2222222222222222222222222222
 
@@ -17,23 +23,24 @@
 
 
 
-// let a = +prompt('Enter your number');
+let a = +prompt('Enter your number');
 
 
 
-// function calculate(){
-//     let arr = [];
-// for (i = 0; i <= a; i++){
-//     arr[i]=i;
-// }
-//     let sum = 0;
-//     for (i = 0; i <= arr.length; i+=3){
-//         sum+=i;
-//     }
+function calculate(){
+    let arr = [];
+for (let i = 1; i <= a; i++){
+    arr[i]=i;
+}
+    // console.log(arr)
+    let sum = 0;
+    for (i = 1; i <= arr.length; i+=3){
+        sum+=i;
+    }
 
-//     console.log(sum);
-// }
-// calculate();
+    console.log(sum);
+}
+calculate();
 
 
 
@@ -43,20 +50,20 @@
 
 // 44444444444444444444444444444
 
-let a = +prompt('количество строк');
-let b = +prompt('Количетсво повторов');
+// let a = +prompt('количество строк');
+// let b = +prompt('Количетсво повторов');
 
-let d = '#.';
-let y = '.#';
-let string = [];
-let arr = [];
-for (i = 1; i <= a; i++){
-    arr[i] = i;
-    if (arr[i]%2!=0){
-        string = string + d.repeat(b) + '\n';
-    } else {
-        string = string + y.repeat(b) + '\n';
-    }
-}
+// let d = '#.';
+// let y = '.#';
+// let string = [];
+// let arr = [];
+// for (i = 1; i <= a; i++){
+//     arr[i] = i;
+//     if (arr[i]%2!=0){
+//         string = string + d.repeat(b) + '\n';
+//     } else {
+//         string = string + y.repeat(b) + '\n';
+//     }
+// }
 
-console.log(string);
+// console.log(string);
